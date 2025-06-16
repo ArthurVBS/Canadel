80a1440e2e223a79527b705f8c105df9e081f551
Arthur Silva <arthurvini2703@gmail.com>
2025-06-16 18:40:41 -0300

    Initial commit
    - Added initial root files
    - Added initial backend files
    - Added initial frontend files

---
+++ backend/src/main/java/com/canadel/CanadelApplication.java
@@ -0,0 +1,13 @@
+package com.canadel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanadelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanadelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/canadel/CanadelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canadel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanadelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
