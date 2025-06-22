6b0b0d363aba228336e14a71c7987fddacc1a77c
Arthur Silva <arthurvini2703@gmail.com>
2025-06-22 08:15:46 -0300

    feat: Increase backend validation
    - Added validations to add and update endpoints
    - Added unit tests

--- backend/src/main/java/com/canadel/constant/ExceptionMessages.java
+++ backend/src/main/java/com/canadel/constant/ExceptionMessages.java
@@ -2,6 +2,7 @@ package com.canadel.constant;
 
 public class ExceptionMessages {
   public static final String PRODUCT_NOT_FOUND = "Product not found";
+  public static final String PRODUCT_WITH_EMPTY_VALUES = "Product has empty values";
   public static final String SOMETHING_WENT_WRONG =
       "Something went wrong! Please contact the admin.";
 }
